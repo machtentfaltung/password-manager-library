5d8e71d0d47a9e02a9b166900cd95530e5dd3a6c
Matei Pralea <matei@pralea.me>
2025-11-26 20:19:40 +0200

    feat: initial commit

---
+++ Field.cs
@@ -0,0 +1,17 @@
+// Copyright © 2025 Matei Pralea <matei@pralea.me>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+namespace PasswordManager;
+
+public enum FieldType 
+{
+    Hidden,
+    Plain,
+}
+
+public class Field(string title, FieldType type, string content)
+{
+    public string Title { get; set; } = title;
+    public FieldType Type { get; set; } = type;
+    public string Content { get; set; } = content;
+}
---
+++ Manager.cs
@@ -0,0 +1,30 @@
+// Copyright © 2025 Matei Pralea <matei@pralea.me>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+using System;
+
+namespace PasswordManager;
+
+public class Manager
+{
+    public List<Account> Accounts { get; private set; }
+
+    public Manager()
+    {
+        Accounts = new List<Account>();
+    }
+
+    public void AddAccount(Account account)
+    {
+        if (Accounts.Contains(account))
+            throw new Exception("Account already exists");
+        Accounts.Add(account);
+    }
+
+    public void RemoveAccount(Account account)
+    {
+        if (!Accounts.Contains(account))
+            throw new Exception("Account not found");
+        Accounts.Remove(account);
+    }
+}
